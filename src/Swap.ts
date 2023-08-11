5aa73902a23d9847620b64fb0e649b9a918f926d
Jairus Tanaka <jairus.v.tanaka@outlook.com>
2023-08-11 11:01:11 -0700

    new swap parser

--- src/Swap.ts
+++ src/Swap.ts
@@ -30,10 +30,12 @@ export class Transaction {
     id: string = "";
     blockNumber: string = "";
 }
-
 export class SwapParser {
     private offset: u32 = 18;
-    constructor(private swapTextData: string) {}
+    private max_len: u32;
+    constructor(private swapTextData: string) {
+        this.max_len = u32(this.swapTextData.length - 50);
+    }
     parseTransactionID(): string {
         const result = this.swapTextData.slice(this.offset + 7, this.offset + 73);
         this.offset = this.swapTextData.indexOf("\"", this.offset + 79);
@@ -68,7 +70,7 @@ export class SwapParser {
     }
     parseTick(): string {
         const end_index = this.swapTextData.indexOf("\"", this.offset += 11);
-        const result = this.swapTextData.slice(index, this.swapTextData.indexOf("\"", this.offset + 1));
+        const result = this.swapTextData.slice(this.offset, this.swapTextData.indexOf("\"", this.offset + 1));
         this.offset = end_index;
         return result;
     }
@@ -96,94 +98,15 @@ export class SwapParser {
         swap.transaction.blockNumber = this.parseBlockNumber();
         swap.tick = this.parseTick();
         swap.sqrtPriceX96 = this.parseSqrtPriceX96();
-        if (this.offset > this.swapTextData.length - 50) return null;
+        if (this.offset > this.max_len) return null;
         return swap;
     }
 }
 
-let parse_offset = 18;
-
-export function parseSwaps(this.swapTextData: string): Swap[] {
-    //let lastOffset = 0;
-    const swaps = new Array<Swap>();
-    while (parse_offset < this.swapTextData.length - 50) {
-        const swap = initSwap();
-        parse_offset = parseSwap(this.swapTextData, swap, parse_offset);
-        /*if (lastOffset === parse_offset) {
-            console.log(`Encountered error parsing at [${this.swapTextData.charAt(parse_offset)}] ${this.swapTextData.slice(parse_offset - 10, parse_offset + 10)}`);
-            break;
-        } else {
-            lastOffset = parse_offset;
-        }*/
-        swaps.push(swap);
-    }
-    parse_offset = 18;
-    return swaps;
-}
-
-export function parseSwap(this.swapTextData: string, swap: Swap, offset: i32): i32 {
-    offset = parseIDs(this.swapTextData, swap, offset);
-    offset = parseTimestamp(this.swapTextData, swap, offset);
-    offset = parseAmount0(this.swapTextData, swap, offset);
-    offset = parseAmount1(this.swapTextData, swap, offset);
-    offset = parseBlockNumber(this.swapTextData, swap, offset);
-    offset = parseTick(this.swapTextData, swap, offset);
-    offset = parseSqrtPriceX96(this.swapTextData, swap, offset);
-    return offset;
-}
-
-// @ts-ignore
-@inline export function parseIDs(this.swapTextData: string, swap: Swap, index: i32): i32 {
-    swap.transaction.id = this.swapTextData.slice(this.offset + 7, this.offset + 73);
-    const end_index = this.swapTextData.indexOf("\"", this.offset + 79);
-    swap.id = swap.transaction.id + this.swapTextData.slice(this.offset + 73, this.offset);
-    return end_index;
-}
-
-// @ts-ignore
-@inline export function parseTimestamp(this.swapTextData: string, swap: Swap, index: i32): i32 {
-    const end_index = this.swapTextData.indexOf("\"", this.offset + 25);
-    swap.timestamp = this.swapTextData.slice(this.offset + 15, end_this.offset);
-    return end_index;
-}
-
-// @ts-ignore
-@inline export function parseAmount0(this.swapTextData: string, swap: Swap, index: i32): i32 {
-    const end_index = this.swapTextData.indexOf("\"", this.offset += 13);
-    swap.amount0 = this.swapTextData.slice(index, this.swapTextData.indexOf("\"", this.offset + 1));
-    return end_index;
-}
-
-// @ts-ignore
-@inline export function parseAmount1(this.swapTextData: string, swap: Swap, index: i32): i32 {
-    const end_index = this.swapTextData.indexOf("\"", this.offset += 13);
-    swap.amount1 = this.swapTextData.slice(index, this.swapTextData.indexOf("\"", this.offset + 1));
-    return end_index;
-}
-
-// @ts-ignore
-@inline export function parseBlockNumber(this.swapTextData: string, swap: Swap, index: i32): i32 {
-    const end_index = this.swapTextData.indexOf("\"", this.offset += 106);
-    swap.transaction.blockNumber = this.swapTextData.slice(index, this.swapTextData.indexOf("\"", this.offset + 1));
-    return end_index;
-}
-
-// @ts-ignore
-@inline export function parseTick(this.swapTextData: string, swap: Swap, index: i32): i32 {
-    const end_index = this.swapTextData.indexOf("\"", this.offset += 11);
-    swap.tick = this.swapTextData.slice(index, this.swapTextData.indexOf("\"", this.offset + 1));
-    return end_index;
-}
-
-// @ts-ignore
-@inline export function parseSqrtPriceX96(this.swapTextData: string, swap: Swap, index: i32): i32 {
-    const end_index = this.swapTextData.indexOf("\"", this.offset + 18);
-    swap.sqrtPriceX96 = this.swapTextData.slice(this.offset + 18, end_this.offset);
-    return end_this.offset + 3;
-}
-
 export function initSwap(): Swap {
+    // @ts-ignore
     const swap = changetype<Swap>(__new(offsetof<Swap>(), idof<Swap>()));
+    // @ts-ignore
     swap.transaction = changetype<Transaction>(__new(offsetof<Transaction>(), idof<Transaction>()));
     return swap;
 }
--- src/test.ts
+++ src/test.ts
@@ -1,12 +1,9 @@
-import { parseSwaps } from "./Swap";
-import { swaps } from "./swap_data";
+import { SwapParser } from "./Swap";
+const swaps = '{"data":{"swaps":[{"id":"0x048ca158346638ec4fdde3ed2d48bef4780e94b9f6e8cebdc7f4a8010f556971#149253","timestamp":"1674648456","amount0":"-0.101898876022884747","amount1":"157.975015","transaction":{"id":"0x048ca158346638ec4fdde3ed2d48bef4780e94b9f6e8cebdc7f4a8010f556971","blockNumber":"38508153"},"tick":"-202888","sqrtPriceX96":"3115031348980421727231748"},{"id":"0xcaff424317352b1aac3f55086d58cf723df2d2187acbec0bbe541c7cb22a04fc#149254","timestamp":"1674648458","amount0":"-0.198048461170833148","amount1":"307.144059","transaction":{"id":"0xcaff424317352b1aac3f55086d58cf723df2d2187acbec0bbe541c7cb22a04fc","blockNumber":"38508154"},"tick":"-202883","sqrtPriceX96":"3115749614570460988489386"}]}}'
+const parser = new SwapParser(swaps);
 
-const start_time = Date.now();
-let parsed_swaps = parseSwaps(swaps);
-const end_time = Date.now();
-for (let i = 0; i < parsed_swaps.length; i++) {
-    const swap = parsed_swaps[i];
-    /*console.log(
+const swap = parser.parseNextSwap()!;
+console.log(
 `{
 "id": "${swap.id}",
 "timestamp": "${swap.timestamp}",
@@ -18,7 +15,4 @@ for (let i = 0; i < parsed_swaps.length; i++) {
 },
 "tick": "${swap.tick}",
 "sqrtPriceX96": "${swap.sqrtPriceX96}"
-}`);*/
-}
-console.log(`Parsed ${parsed_swaps.length} swaps in approximately ${end_time - start_time}ms`);
-console.log(`Size: ${swaps.length} bytes`)
+}`);
